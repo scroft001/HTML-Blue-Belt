b875609479a277888a06e69bad53583b27c4fae2
Steven Croft <scroft001@gmail.com>
2022-01-06 14:43:47 -0800

    Added project 2

---
+++ Project 2/script.js
@@ -0,0 +1,5 @@
+function troll() {
+	document.getElementById('textChange').innerHTML = 'Thief! You have been hacked and have 1,000 viruses';
+	document.getElementById('imageChange').src = 'virus.jpg';
+	document.getElementById('page').style.background = 'red';
+}
